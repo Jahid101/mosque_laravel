5d014df2140ebea99ef236bdc7d5621b1c62943c
Jahid <jahidhasananik.official@gmail.com>
2021-03-15 16:53:46 +0600

    done for today

--- resources/views/backend/home.blade.php
+++ resources/views/backend/home.blade.php
@@ -21,16 +21,18 @@
 
 
             <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-                <div
+                {{-- <div
                     class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                     <h1 class="h2 m-auto">ABC Mosque</h1>
-                </div>
+                </div> --}}
 
-
+                <div class="m-3">
                     @yield('content')
+                </div>
                 
 
-            {{-- @include('backend.partial.sidebarright') --}}
+
+                {{-- @include('backend.partial.sidebarright') --}}
 
 
 
--- resources/views/backend/partial/header.blade.php
+++ resources/views/backend/partial/header.blade.php
@@ -4,12 +4,15 @@
         data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
-    <input class="form-control form-control-dark w-50" type="text" placeholder="Search" aria-label="Search">
-    <button class="btn btn-dark">Search</button>
+    {{-- <input class="form-control form-control-dark w-50" type="text" placeholder="Search" aria-label="Search">
+    <button class="btn btn-dark">Search</button> --}}
+    <h1 style="color: white"><strong>ABC Mosque</strong></h1>
+
     <ul class="navbar-nav px-3">
         <li class="nav-item text-nowrap">
             <a class="nav-link active" href="#">
-                <button class="btn btn-danger shadow-lg">Sign out</button>
+                <button class="btn btn-danger shadow-lg">Sign in</button>
+                {{-- <button class="btn btn-danger shadow-lg">Sign out</button> --}}
             </a>
         </li>
     </ul>
