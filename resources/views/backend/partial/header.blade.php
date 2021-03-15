a1635d567e034adfd8e583df1fc3f9b0a7756268
Jahid <jahidhasananik.official@gmail.com>
2021-03-15 16:02:31 +0600

    go

--- resources/views/backend/partial/header.blade.php
+++ resources/views/backend/partial/header.blade.php
@@ -1,5 +1,5 @@
 <header class="navbar navbar-dark sticky-top bg-success flex-md-nowrap p-0 shadow m-2 rounded">
-    <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="/home">Home</a>
+    <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 shadow-lg" href="/home">Home</a>
     <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse"
         data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -9,7 +9,7 @@
     <ul class="navbar-nav px-3">
         <li class="nav-item text-nowrap">
             <a class="nav-link active" href="#">
-                <button class="btn btn-danger">Sign out</button>
+                <button class="btn btn-danger shadow-lg">Sign out</button>
             </a>
         </li>
     </ul>
--- resources/views/backend/partial/sidebar.blade.php
+++ resources/views/backend/partial/sidebar.blade.php
@@ -1,25 +1,25 @@
 <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block sidebar collapse">
       <div class="position-sticky pt-3 bg-success mt-3 mb-5 rounded">
         <ul class="nav flex-column">
-          <li class="nav-item">
+          <li class="nav-item shadow-lg">
             <a class="nav-link active" aria-current="page" href={{Route('admin')}}>
               <span data-feather="home"></span>
               <button class="btn btn-success">Admin</button>
             </a>
           </li>
-          <li class="nav-item">
+          <li class="nav-item shadow-lg">
             <a class="nav-link" href={{Route('donate')}}>
               <span data-feather="file"></span>
               <button class="btn btn-success">Donate</button>
             </a>
           </li>
-          <li class="nav-item">
+          <li class="nav-item shadow-lg">
             <a class="nav-link" href={{Route('event')}}>
               <span data-feather="shopping-cart"></span>
               <button class="btn btn-success">Events</button>
             </a>
           </li>
-          <li class="nav-item">
+          <li class="nav-item shadow-lg">
             <a class="nav-link" href={{Route('announcement')}}>
               <span data-feather="shopping-cart"></span>
               <button class="btn btn-success">Announcement</button>
@@ -32,36 +32,36 @@
             </a>
           </li> --}}
           <li class="nav-item">
-            <a class="nav-link" href= {{Route('fund')}}>
+            <a class="nav-link shadow-lg" href= {{Route('fund')}}>
               <span data-feather="bar-chart-2"></span>
               <button class="btn btn-success">Fund</button>
             </a>
           </li>
-          <li class="nav-item">
+          <li class="nav-item shadow-lg">
             <a class="nav-link" href="#">
               <span data-feather="bar-chart-2"></span>
               <button class="btn btn-success">Tablighi</button>
             </a>
           </li>
-          <li class="nav-item">
+          <li class="nav-item shadow-lg">
             <a class="nav-link" href="#">
               <span data-feather="bar-chart-2"></span>
               <button class="btn btn-success">Maktab</button>
             </a>
           </li>
-          <li class="nav-item">
+          <li class="nav-item shadow-lg">
             <a class="nav-link" href= {{Route('food')}}>
               <span data-feather="bar-chart-2"></span>
               <button class="btn btn-success">Food</button>
             </a>
           </li>
-          <li class="nav-item">
+          <li class="nav-item shadow-lg">
             <a class="nav-link" href="#">
               <span data-feather="bar-chart-2"></span>
               <button class="btn btn-success">Mahfil</button>
             </a>
           </li>
-          <li class="nav-item">
+          <li class="nav-item shadow-lg">
             <a class="nav-link" href={{Route('about')}}>
               <span data-feather="layers"></span>
               <button class="btn btn-success">About</button>
