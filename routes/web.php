0c4dc6dfdf425a2f0676704a74c817d86ed8593e
Jahid <jahidhasananik.official@gmail.com>
2021-03-22 15:49:13 +0600

    database connected

--- routes/web.php
+++ routes/web.php
@@ -15,18 +15,6 @@ use App\Http\Controllers\Backend\MaktabController;
 use App\Http\Controllers\Backend\MahfilController;
 
 
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 // Route::get('/home', function () {
 //     return view('backend.home');
 // });
@@ -55,3 +43,5 @@ Route::get('/tablighi',[TablighiController::class,'tablighi'])->name('tablighi')
 Route::get('/maktab',[MaktabController::class,'maktab'])->name('maktab');
 
 Route::get('/mahfil',[MahfilController::class,'mahfil'])->name('mahfil');
+
+Route::post('/donation',[DonateController::class,'list'])->name('donate.list');
